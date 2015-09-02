5b97a4eee1c726e268b95c06fd35008d4ca1f3aa
Stuart Cannon <sc651@grace.ex.ac.uk>
2015-09-02 15:58:55 +0100

    creating dictionary using position as key and list of mutations with total cohort

--- frequency_comparison.py
+++ frequency_comparison.py
@@ -2,33 +2,47 @@
  
 import re
 
-
 #assign infiles and create an outfile 
 with open("hgmd") as infile1, open("result", "w") as outfile:
-#write the variants annotated as disease in hgmd to the outfile 
+#write the variants annotated as disease causing in hgmd to the outfile 
 			for line in infile1:
 				if "Disease causing mutation" in line:
 					outfile.write(line)
 
 #using regular expressions to identify where more than one variant has been identified in a cohort
-p = re.compile('\d*,\d') #make sure match all numbers not just 1-9 
+#^ means at the start, \d means digit for 0-9, + means just one match, , is a literal comma, .* means anythin an unlimited number of times.
+#this therefore returns a match if it has a number comma number and then will greedily include everything else following the match   
+p = re.compile('^\d+,\d.*')
 
-#opening the exac file
+#this looks for one number at the start of the string,the $ means tonly the number gets matched  
+q = re.compile('^\d+$')
+
+my_dict = {'location': 'list'}
+
+#opening the exac file to be read
 with open("exac.frequencies", "r") as file:
 	#looping through each line in the exac file
 	for line in file:
 		#splitting lines into useable columns
 		columns = line.split()
-		#identifying parts that have more than one varient 
+		#identifying parts in exac database that have more than one variant 
 		if p.match(columns[1]):
-			#assigning a row to the variable more_mut if it meets the regular expression
-			more_mut = columns[1]
-			print more_mut
-			num_in_list = len(more_mut)#everytime there is more than one for each of these things do that action
-			print num_in_list
+			#creating a list of each character from the string concatenated with the population column as the final character, split columns[1] by comma
+			#it's necessary to split the columns[2] variable so itself becomes a list 
+			more_mut = columns[1].split(",") + columns[2].split()			
+			#creating a dictionary lookup
+			my_dict[columns[0]] = more_mut			
+		elif q.match(columns[1]):
+			one_mut = columns[1].split() + columns[2].split() 	
+			my_dict[columns[0]] = one_mut
+
+#print my_dict 
+
+			#num_in_list = len(more_mut)#everytime there is more than one for each of these things do that action
+			#print num_in_list
 			#removiing the comma from the identified strings
-			result = re.sub('[,]', '', more_mut)
-			print result
+			#result = re.sub('[,]', '', more_mut)
+			#print result
 			#turning each digit into an integer
 			#need an array within a hash table 
 	#turn into array, put array in hash table with id (first column) as the key - the bit that
@@ -38,9 +52,9 @@ with open("exac.frequencies", "r") as file:
 	#
 	#key = id, the value for this key needs to be an array of 
 	#split by commas to form an array 
-			x = int(result)
-			y = x.split()
-			print y
+			#x = int(result)
+			#y = x.split()
+			#print y
 			#for digit in str(result):
 			#	x = int(digit)
 			#	list_of_integers = list(x)
