e9628796ce28cee0083098830e84cf328dd2cc40
Stuart Cannon <sc651@grace.ex.ac.uk>
2015-09-03 14:21:52 +0100

    finalised putting MAF lists as a value for a dictionary

--- frequency_comparison.py
+++ frequency_comparison.py
@@ -21,18 +21,16 @@ q = re.compile('^\d+$')
 my_dict = {}
 
 #opening the exac file to be read
-with open("test.freq", "r") as file:
+with open("exac.frequencies", "r") as file:
 	#looping through each line in the exac file
 	for line in file:
 		#splitting lines into useable columns
 		columns = line.split()
 		#identifying parts in exac database that have more than one variant 
 		if p.match(columns[1]):
-			print columns[1]
 			#creating a list of each character from the string concatenated with the population column as the final character, split columns[1] by comma
 			#it's necessary to split the columns[2] variable so itself becomes a list 
 			more_mut = columns[1].split(",") + columns[2].split()			
-			print more_mut
 			location = columns[0]
 			MAF_list = [] 
 			for mut in more_mut:
@@ -40,13 +38,10 @@ with open("test.freq", "r") as file:
 				if mut != last:
 					MAF = float(mut)/float(last)
 					MAF_list.append(MAF)
-					print MAF_list 
 					#assigning these matches to a dictionary with postion as a key and the concatenated list as the value
 				my_dict[location] = MAF_list					
 		elif q.match(columns[1]):
-			print columns[1]
 			one_mut = columns[1].split(",") + columns[2].split()
-			print one_mut
 			location = columns[0]
 			MAF_list = []
 			for mut in one_mut:
@@ -54,10 +49,12 @@ with open("test.freq", "r") as file:
 				if mut != last:
 					MAF = float(mut)/float(last)			
 					MAF_list.append(MAF)
-					print MAF_list
-					my_dict[location] = MAF_list
-
+				my_dict[location] = MAF_list
 		else:
-			 print "error" + columns[1]
+			print "error" + columns[1]
 
-print my_dict 
+#print  my_dict
+
+#compare locations of disease causing mutations from result file with dictionary entries
+
+#open the 
