f14938ef9d914b86de828c414e2df914a78f91f7
Stuart Cannon <sc651@grace.ex.ac.uk>
2015-09-09 15:42:10 +0100

    code works and saves to file but needs optimising

--- frequency_comparison.py
+++ frequency_comparison.py
@@ -3,11 +3,12 @@
 import re
 
 #assign infiles and create an outfile 
-with open("hgmd") as infile, open("hgmd_disease", "w") as outfile:
+#with open("hgmd") as infile1, open(exac.frequencies) as infile2, open("hgmd_disease", "w") as outfile:
 #write the variants annotated as disease causing in hgmd to the outfile 
-			for line in infile:
-				if "Disease causing mutation" in line:
-					outfile.write(line)
+#			for line in infile1 and infile2:
+#				line = line.rstrip()
+#				if "Disease causing mutation" and in line:
+#					outfile.write(line)
 
 #using regular expressions to identify where more than one variant has been identified in a cohort
 #^ means at the start, \d means digit for 0-9, + means just one match, , is a literal comma, .* means anythin an unlimited number of times.
@@ -20,36 +21,39 @@ q = re.compile('^\d+$')
 #creating a dictionary which can later be altere
 my_dict = {}
 
-#opening the exac file to be read
-with open("exac.frequencies", "r") as file:
+#opening the exac file to be read the entry 11:11111111 was entered as a positive control to the test file
+with open("exac.frequencies.test", "r") as file:
 	#looping through each line in the exac file
 	for line in file:
+                line = line.rstrip()
+		#print line
 		#splitting lines into useable columns
 		columns = line.split()
 		#identifying parts in exac database that have more than one variant 
 		if p.match(columns[1]):
 			#creating a list of each character from the string concatenated with the population column as the final character, split columns[1] by comma
 			#it's necessary to split the columns[2] variable so itself becomes a list 
-			more_mut = columns[1].split(",") + columns[2].split()			
+			more_mut = columns[1].split(",") + columns[2].split()
 			location = columns[0]
-			MAF_list = [] 
+			MAF_list = []
+			last = more_mut[-1]
 			for mut in more_mut:
-				last = more_mut[-1]
+				#last = more_mut[-1]
 				if mut != last:
 					MAF = float(mut)/float(last)
 					MAF_list.append(MAF)
 					#assigning these matches to a dictionary with postion as a key and the concatenated list as the value
-				my_dict[location] = MAF_list					
+					my_dict[location] = MAF_list				
 		elif q.match(columns[1]):
 			one_mut = columns[1].split(",") + columns[2].split()
 			location = columns[0]
 			MAF_list = []
+			last = one_mut[-1]
 			for mut in one_mut:
-				last = one_mut[-1]
 				if mut != last:
 					MAF = float(mut)/float(last)			
 					MAF_list.append(MAF)
-				my_dict[location] = MAF_list
+					my_dict[location] = MAF_list
 		else:
 			print "error" + columns[1]
 
@@ -63,24 +67,27 @@ with open("exac.frequencies", "r") as file:
 #iterate thrrough hgmd_disease lines using location
 
 #open hgmd_disease file
-with open("hgmd_disease", "r") as infile, open("hgmd_and_exac", "w") as outfile:
+with open("hgmd_disease", "r") as infile, open("test.result", "wa") as outfile:
 	#iterate through the lines
 	for line in infile:
 		#split each line into columns 
 		columns = line.split()
 		#assign column [0] to variable k
-		k = columns[0]
+		key = columns[0]
 		#if column 0 (location) is in the dictionary keys
-		if k in my_dict.keys():
+		if key in my_dict.keys():
 			#assign the values of that key to variable v
-			v = (my_dict[k])
+			#print k
+			value = (my_dict[key])
 			#for each value in the list relating to identified keys 
-			for each in v:
+			for each in value:
 				#if each value is greater than 0.01
 				if each > 0.01:
-					print each
+					print key
+					print str(each) + "written"
 					outfile.write(line + " " + str(each) + "\n")	 
 
+outfile.close()
 #output file to include exac colunms followed by hgmd_disease coulmns
 
 
